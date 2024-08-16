99628a80aafbfdbddb14465809cc242dd92c1b00
pryntl <pryntl2007@gmail.com>
2024-08-16 16:53:42 +0330

    feat [java script]
    added a function which counts how many times a word is repeated in a string. also finding the index of each.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+function indexOfTarget(string, target) {
+  let index = [];
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    let partOfString = string.slice(i, i + target.length);
+    if (partOfString === target) {
+      index.push(i);
+      count++;
+    }
+  }
+  return {
+    index,
+    count,
+  };
+}
+console.log(indexOfTarget("an ant and an elephant", "an"));
